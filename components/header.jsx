6a7f65228fc53424ba9640d9acd6fc913e940a85
Sean Riesenberger <sean.riesenberger@student.swic.edu>
2025-05-16 04:43:20 -0500

    update header for Rick and Morty

--- components/header.jsx
+++ components/header.jsx
@@ -3,10 +3,10 @@ export default function Header() {
     <header>
       <h1
         className={
-          'mb-10 bg-gradient-to-r from-blue-500 to-red-500 bg-clip-text text-center text-5xl font-bold text-transparent uppercase'
+          'mb-10 bg-gradient-to-r from-green-600 to-green-300 bg-clip-text text-center text-5xl font-bold text-transparent uppercase'
         }
       >
-        Star Wars Characters
+        Rick and Morty Verse
       </h1>
     </header>
   )
