52d1309e4cb6ea498ff631bc63c902de0badc908
Sean Riesenberger <sean.riesenberger@student.swic.edu>
2025-05-16 04:41:30 -0500

    Replace results in card.jsx for Rick and Morty

--- components/card.jsx
+++ components/card.jsx
@@ -1,11 +1,10 @@
-export default function Card({ person }) {
+export default function Card({ character }) {
   return (
     <section className="overflow-hidden rounded-lg bg-white p-4 shadow-md transition-transform duration-300 hover:scale-105">
-      <h2 className="text-xl font-semibold text-gray-800">{person.name}</h2>
-      <p className="mt-2 text-gray-600">Height: {person.height} cm</p>
-      <p className="mt-2 text-gray-600">Mass: {person.mass} kg</p>
-      <p className="mt-2 text-gray-600">Hair Color: {person.hair_color}</p>
-      <p className="mt-2 text-gray-600">Skin Color: {person.skin_color}</p>
+      <h2 className="text-xl font-semibold text-gray-800">{character.name}</h2>
+      <p className="mt-2 text-gray-600">Dead or Alive: {character.status} cm</p>
+      <p className="mt-2 text-gray-600">Species: {character.species} kg</p>
+      <p className="mt-2 text-gray-600">Type: {character.type}</p>
     </section>
   )
 }
